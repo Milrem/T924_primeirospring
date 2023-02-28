42cd135626930c31848b48e57e1c280496aa52cc
milrem <cris.a.neves@gmail.com>
2023-02-27 22:14:27 -0300

    Criação do dto Area

---
+++ src/main/java/neves/cristiano/primeirospring/dto/Area.java
@@ -0,0 +1,14 @@
+package neves.cristiano.primeirospring.dto;
+
+import lombok.Data;
+
+@Data
+public class Area {
+    private Comodo comodo;
+    public Float getAreaPiso() {
+        return comodo.getLargura() * comodo.getComprimento();
+    }
+    public Float getAreaParede() {
+        return (comodo.getLargura() * comodo.getAltura() + comodo.getComprimento() * comodo.getAltura()) * 2;
+    }
+}
